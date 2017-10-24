2c36fa46a3653e86c938bf335a8e5077fad434a2
GitInMaRepo <oliver.s.roth@gmail.com>
2017-10-24 14:20:38 +0200

    Server is running

---
+++ BC3Server/BC3Server/Program.cs
@@ -0,0 +1,21 @@
+﻿using Nancy.Hosting.Self;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BC3Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var host = new NancyHost(new Uri("http://localhost:8088")))
+            {
+                host.Start();
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ BC3Server/BC3Server/Server.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BC3Server
+{
+    public class Server : Nancy.NancyModule
+    {
+        public Server()
+        {
+            Get["/"] = _ => "Hello World!";
+        }
+    }
+}
